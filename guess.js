5c9ae0fa47d9a6c5d01bae01086329fc2b31b41f
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2020-09-22 16:07:51 +0300

    update all files

--- guess.js
+++ guess.js
@@ -1,16 +1,54 @@
+
+let input = document.getElementById("text");
+let button = document.getElementById("btn");
+let statement = document.getElementById("statement");
+let tester = document.getElementById("tester");
+let weite = document.getElementById("weite");
+
 let rndm = Math.floor(Math.random()*101); 
 /* Rasgele sayı üretmek için Math (0 dahil 1   hariç)*/
-document.getElementById("btn").addEventListener("click",check)
+
+button.addEventListener("click",check);
+
+let counter = 0;
 
 function check() {
-    let num = document.getElementById("text").value;
-    if (rndm == num) {
-        alert("bildin");    
-    }else if (rndm > num) {
-        alert("biraz yukarı");
-    }else {
-        alert("biraz aşağıya");
+   
+    let num = Number(input.value);
+    
+    if (isNaN(num) || num == "" ) {
+
+      statement.innerText = "Are you sure that is a number ?";
+      return;    
     } 
+
+    if (rndm == num){
+        counter++;
+        alert(`You are the winner :smile:. You guest after ${counter} times!`);
+        
+        if (confirm("You wanna play again")){
+            window.location.reload();
+
+        }else{
+            window.close();
+        }
+
+        
+    }else if(rndm>num){
+        statement.innerText = "I am thinking about Higher Number";
+
+    }else {
+        statement.innerText ="I am thinking about Lower Number";
+    }
+
+    counter++;
+    weite.innerText= "Weite the number";
+    tester.innerText=`This is your test number:${counter}`;
+
+    input.value ="";
+    input.focus();
+  
+
 }
 
-console.log(rndm);
+
